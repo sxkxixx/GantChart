0f6bdb753166d180d1812b0f3559d3b1d6241bd1
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-04-19 20:05:15 +0500

    cors + requirements

--- GantBackend/GantBackend/settings.py
+++ GantBackend/GantBackend/settings.py
@@ -51,15 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000'
-]
-
-CORS_ALLOWED_METHODS = [
-    'GET',
-    'POST',
-    'DELETE'
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'GantBackend.urls'
 
