f0a5e5c36afc36f4bf15c6acdb1a30c33f8da629
Mag1strVark <molot2101@gmail.com>
2023-07-07 00:21:45 +0500

    добавил дизайн в карточки канбана

--- GantFrontend/src/components/KanbanChart/Card/Card.jsx
+++ GantFrontend/src/components/KanbanChart/Card/Card.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import s from './Card.module.css'
+import {ReactComponent as User} from "../../../assets/img/User.svg";
+import {ReactComponent as Cal} from "../../../assets/img/calendar.svg";
 
 const Card = ({
                   items,
@@ -17,7 +20,11 @@ const Card = ({
                 onDragOver={(e) => dragOverHandler(e)}
                 onDragStart={(e) => dragStartHandler(e, board, items)}
             >
-                {items.name}
+               <span className={s.title}>{items.name}</span>
+                <span className={s.project}>{items.project}</span>
+                <span className={s.team}>#{items.team}</span>
+                <span className={s.user}><User/>{items.user}</span>
+                <span className={s.deadline}><Cal/>{items.deadline}</span>
             </div>
         </>
     );
