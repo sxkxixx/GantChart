8cda0f52c1dbf015934acdc94d7be3ef46328f0a
Mag1strVark <molot2101@gmail.com>
2023-07-08 19:02:54 +0500

    исправил отображение отрезков времени

--- GantFrontend/src/components/GanttChart/GanttTable/GanttTaskRow/GanttTaskRow.jsx
+++ GantFrontend/src/components/GanttChart/GanttTable/GanttTaskRow/GanttTaskRow.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import s from './GanttTaskRow.module.css';
 
 const GanttTaskRow = ({
@@ -18,20 +18,13 @@ const GanttTaskRow = ({
     const isCollapsed = collapsedTasks.includes(id);
     const currentIndentLevel = indentLevel;
 
-    // const [startIndex, setStartIndex] = useState(
-    //     Math.round((new Date(taskStartDate) - startDate) / (1000 * 60 * 60 * 24))
-    // );
-    // const [endIndex, setEndIndex] = useState(
-    //     Math.round((new Date(taskEndDate) - startDate) / (1000 * 60 * 60 * 24))
-    // );
+    const startIndex = Math.round(
+        (new Date(taskStartDate) - startDate) / (1000 * 60 * 60 * 24)
+    );
 
-    const startIndex = (
-        Math.round((new Date(taskStartDate) - startDate) / (1000 * 60 * 60 * 24))
-    )
-
-    const endIndex = (
-        Math.round((new Date(taskEndDate) - startDate) / (1000 * 60 * 60 * 24))
-    )
+    const endIndex = Math.round(
+        (new Date(taskEndDate) - startDate) / (1000 * 60 * 60 * 24)
+    );
 
     const getCellStyle = (indentLevel) => {
         let colorClass;
@@ -42,96 +35,32 @@ const GanttTaskRow = ({
         }
 
         return `${colorClass}`;
-    }
+    };
 
-    // const handleDragStartIndex = (event) => {
-    //     handleDragIndex(event, startIndex, setStartIndex);
-    // };
-    //
-    // const handleDragEndIndex = (event) => {
-    //     handleDragIndex(event, endIndex, setEndIndex);
-    // };
-    //
-    // const handleDragIndex = (event, currentIndex, setCurrentIndex) => {
-    //     const initialMouseX = event.pageX
-    //     const initialIndex = currentIndex
-    //
-    //     const handleMouseMove = (event) => {
-    //         const deltaX = event.pageX - initialMouseX
-    //
-    //         const currentIndexDelta = Math.round(
-    //             (deltaX / event.target.offsetWidth) * projectDurationInDays
-    //         );
-    //
-    //         const newStartIndex = Math.max(
-    //             Math.min(initialIndex + currentIndexDelta, endIndex),
-    //             0
-    //         )
-    //
-    //         const newEndIndex = Math.min(
-    //             Math.max(endIndex + currentIndexDelta, startIndex),
-    //             projectDurationInDays - 1
-    //         )
-    //
-    //         setCurrentIndex(currentIndex === startIndex ? newStartIndex : newEndIndex);
-    //     };
-    //
-    //     const handleMouseUp = () => {
-    //         document.removeEventListener('mousemove', handleMouseMove);
-    //         document.removeEventListener('mouseup', handleMouseUp);
-    //     };
-    //
-    //     document.addEventListener('mousemove', handleMouseMove);
-    //     document.addEventListener('mouseup', handleMouseUp,  { once: true });
-    // };
-
-    const dateCells = [];
-    const activeCellIndex = new Set();
-    for (let i = 0; i < projectDurationInDays; i++) {
-        const isActiveCell = i >= startIndex && i <= endIndex;
-        if (isActiveCell) {
-            activeCellIndex.add(i);
+    const renderEmptyCells = (start, end) => {
+        const emptyCells = [];
+        for (let i = start; i <= end; i++) {
+            emptyCells.push(
+                <div className={`${s.cellWrapper} ${s.emptyCell}`} />
+            );
         }
-        const style = `${s.cell} ${
-            isActiveCell ? getCellStyle(currentIndentLevel) : ''
-        }`;
-        const cellClassName =
-            i === startIndex
-                ? `${style} ${s.startCell}`
-                : i === endIndex
-                    ? `${style} ${s.endCell}`
-                    : i > startIndex && i < endIndex
-                        ? `${style} ${s.middleCell}`
-                        : style;
-        dateCells.push(
-            <div className={cellClassName} key={`${id}-${i}`}>
-                {isActiveCell && <div
-                    className={s.isActiveCellOverlay}
-                    // onMouseDown={
-                    //     i === startIndex
-                    //         ? handleDragStartIndex
-                    //         : i === endIndex
-                    //             ? handleDragEndIndex
-                    //             : undefined
-                    // }
-                />
-                }
-            </div>
-        );
-    }
+        return emptyCells;
+    };
 
+    const renderActiveCell = (start, end) => {
+        return (
+            <div
+                className={`${s.activeCellWrapper} ${getCellStyle(currentIndentLevel)}`}
+                style={{ left: `${start * 100}px`, width: `${(end - start + 1) * 100}px` }}
+            />
+        );
+    };
 
     return (
         <>
             <div className={s.row} key={id}>
-                {dateCells.map((cell, index) => (
-                    <div
-                        className={s.cellWrapper}
-                        key={`${id}-cell-${index}`}
-                    >
-                        {cell}
-                    </div>
-                ))}
+                {renderEmptyCells(0, projectDurationInDays - 1)}
+                {renderActiveCell(startIndex, endIndex)}
             </div>
             {!isCollapsed &&
                 children &&
