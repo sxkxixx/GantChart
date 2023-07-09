9821dee08e368f57cbcde8c692a8ed9423279fbc
Mag1strVark <molot2101@gmail.com>
2023-07-09 22:22:46 +0500

    реализовал функционал перетаскивания задач на самой диаграмме но надо сделать валидацию

--- GantFrontend/src/components/GanttChart/GanttTable/GanttTaskRow/GanttTaskRow.jsx
+++ GantFrontend/src/components/GanttChart/GanttTable/GanttTaskRow/GanttTaskRow.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './GanttTaskRow.module.css';
 import {editDates} from "../../../../services/task";
 
@@ -19,15 +19,43 @@ const GanttTaskRow = ({
 
     const isCollapsed = collapsedTasks.includes(id);
     const currentIndentLevel = indentLevel;
-    const [startDragOffset, setStartDragOffset] = useState(0);
-    const [endDragOffset, setEndDragOffset] = useState(0);
+
     const [startIndex, setStartIndex] = useState(
         Math.round((new Date(taskStartDate) - startDate) / (1000 * 60 * 60 * 24))
     );
     const [endIndex, setEndIndex] = useState(
         Math.round((new Date(taskEndDate) - startDate) / (1000 * 60 * 60 * 24))
     );
+    const [isDragged, setIsDragged] = useState(false); // New state variable
 
+    const handleStartEndIndexesChange = () => {
+        const newStartDate = new Date(startDate);
+        newStartDate.setDate(newStartDate.getDate() + startIndex);
+        const newEndDate = new Date(startDate);
+        newEndDate.setDate(newEndDate.getDate() + endIndex);
+
+        const formattedStartDate = newStartDate.toISOString().split('T')[0];
+        const formattedEndDate = newEndDate.toISOString().split('T')[0];
+
+        const updatedTask = {
+            ...task,
+            planned_start_date: formattedStartDate,
+            planned_final_date: formattedEndDate,
+            deadline: deadTask,
+        };
+
+        editDates(id, updatedTask);
+    };
+
+    useEffect(() => {
+        if (isDragged) {
+            const debounceTimer = setTimeout(handleStartEndIndexesChange, 250);
+
+            return () => {
+                clearTimeout(debounceTimer);
+            };
+        }
+    }, [startIndex, endIndex, isDragged]);
 
     const getCellStyle = (indentLevel) => {
         let colorClass;
@@ -41,84 +69,49 @@ const GanttTaskRow = ({
     };
 
     const handleStartDateDragStart = (event) => {
-        const initialOffset =
-            event.clientX - event.target.getBoundingClientRect().left;
-        setStartDragOffset(0);
-        document.addEventListener('mousemove', handleStartDateDrag, true);
-        document.addEventListener('mouseup', handleStartDateDragEnd, true);
-        event.preventDefault();
-    };
+        const initialX = event.clientX;
 
-    const handleStartDateDrag = (event) => {
-        const newOffset = Math.round(
-            (event.clientX - event.target.getBoundingClientRect().x) / 100
-        );
-        const newStartIndex = startIndex + newOffset;
+        const handleMouseMove = (event) => {
+            const diff = Math.round((event.clientX - initialX) / 100);
+            const newStartIndex = startIndex + diff;
 
-        // Ensure the newStartIndex stays within valid bounds
-        const minIndex = 0;
-        const maxIndex = endIndex - 1;
-        const clampedIndex = Math.min(Math.max(newStartIndex, minIndex), maxIndex);
+            if (newStartIndex >= 0 && newStartIndex <= endIndex) {
+                setStartIndex(newStartIndex);
+                setIsDragged(true); // Set the dragged state to true
+            }
+        };
 
-        setStartIndex(clampedIndex);
-        setStartDragOffset(clampedIndex - startIndex);
-        event.preventDefault();
-    };
+        const handleMouseUp = () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+            setIsDragged(false); // Reset the dragged state to false
+        };
 
-    const handleStartDateDragEnd = () => {
-        document.removeEventListener('mousemove', handleStartDateDrag, true);
-        document.removeEventListener('mouseup', handleStartDateDragEnd, true);
-
-        const newStartDate = new Date(startDate);
-        newStartDate.setDate(newStartDate.getDate() + startIndex);
-        const newEndDate = new Date(startDate);
-        newEndDate.setDate(newEndDate.getDate() + endIndex);
-
-        editDates(id, {
-            planned_start_date: newStartDate.toISOString().split('T')[0],
-            planned_final_date: newEndDate.toISOString().split('T')[0],
-            deadline: deadTask
-        });
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
     };
 
     const handleEndDateDragStart = (event) => {
-        const initialOffset =
-            event.clientX - event.target.getBoundingClientRect().right;
-        setEndDragOffset(0);
-        document.addEventListener('mousemove', handleEndDateDrag, true);
-        document.addEventListener('mouseup', handleEndDateDragEnd, true);
-        event.preventDefault();
-    };
+        const initialX = event.clientX;
 
-    const handleEndDateDrag = (event) => {
-        const newOffset = Math.round(
-            (event.clientX - event.target.getBoundingClientRect().right + 100) / 100
-        );
-        const newEndIndex = endIndex + newOffset;
+        const handleMouseMove = (event) => {
+            const diff = Math.round((event.clientX - initialX) / 100);
+            const newEndIndex = endIndex + diff;
 
-        const minIndex = startIndex + 1;
-        const maxIndex = projectDurationInDays - 1;
-        const clampedIndex = Math.min(Math.max(newEndIndex, minIndex), maxIndex);
+            if (newEndIndex >= startIndex && newEndIndex < projectDurationInDays) {
+                setEndIndex(newEndIndex);
+                setIsDragged(true); // Set the dragged state to true
+            }
+        };
 
-        setEndIndex(clampedIndex);
-        setEndDragOffset(clampedIndex - endIndex);
-        event.preventDefault();
-    };
+        const handleMouseUp = () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+            setIsDragged(false); // Reset the dragged state to false
+        };
 
-    const handleEndDateDragEnd = () => {
-        document.removeEventListener('mousemove', handleEndDateDrag, true);
-        document.removeEventListener('mouseup', handleEndDateDragEnd, true);
-
-        const newStartDate = new Date(startDate);
-        newStartDate.setDate(newStartDate.getDate() + startIndex);
-        const newEndDate = new Date(startDate);
-        newEndDate.setDate(newEndDate.getDate() + endIndex);
-
-        editDates(id, {
-            planned_start_date: newStartDate.toISOString().split('T')[0],
-            planned_final_date: newEndDate.toISOString().split('T')[0],
-            deadline: deadTask
-        });
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
     };
 
     const renderEmptyCells = (start, end) => {
